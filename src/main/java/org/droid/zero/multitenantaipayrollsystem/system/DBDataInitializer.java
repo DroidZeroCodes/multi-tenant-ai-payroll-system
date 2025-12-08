bde120c566a29a2d14fbc21999acf1efc470c617
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-12-08 16:40:16 +0800

    feat: Seed initial user data in dev DB initializer
    Adds logic to the DBDataInitializer to create a default user
    during application startup in the 'dev' profile. This user
    is assigned 'SUPER_ADMIN' and 'EMPLOYEE' roles and linked
    to the 'origin' tenant.
    
    This change facilitates development and testing by providing
    pre-populated user data for the application.

--- src/main/java/org/droid/zero/multitenantaipayrollsystem/system/DBDataInitializer.java
+++ src/main/java/org/droid/zero/multitenantaipayrollsystem/system/DBDataInitializer.java
@@ -4,10 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.droid.zero.multitenantaipayrollsystem.tenant.Tenant;
 import org.droid.zero.multitenantaipayrollsystem.tenant.TenantRepository;
+import org.droid.zero.multitenantaipayrollsystem.user.User;
+import org.droid.zero.multitenantaipayrollsystem.user.UserRepository;
+import org.droid.zero.multitenantaipayrollsystem.user.UserRole;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 @Slf4j
 @Component
@@ -16,18 +21,32 @@ import org.springframework.stereotype.Component;
 public class DBDataInitializer {
 
     private final TenantRepository tenantRepository;
+    private final UserRepository userRepository;
 
     @EventListener(ApplicationReadyEvent.class)
     public void devStartup() {
         log.info("✅ Dev application is ready");
         log.info("🚩 Starting Database initialization...");
 
+        log.info("➡️ Seeding Tenant Data...");
         Tenant tenant = new Tenant();
         tenant.setName("origin");
         tenant.setEmail("orgin@email.com");
         tenant.setPhone("+63 9123456789");
         tenant.setIndustry("Technology");
         tenantRepository.save(tenant);
+        log.info("✅ Successfully Created Tenant Data");
+
+        log.info("➡️ Seeding User Data...");
+        User user = new User();
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setEmail("jDoe@email.com");
+        user.setPassword("password");
+        user.setRole(Set.of(UserRole.SUPER_ADMIN, UserRole.EMPLOYEE));
+        user.setTenant(tenant);
+        userRepository.save(user);
+        log.info("✅ Successfully Created User Data");
 
         log.info("✅ Database initialization completed");
     }
