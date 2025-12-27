756efeaf04decb4304d46e9ce79ebc7e194f9dff
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-12-27 15:19:18 +0800

    feat(header-utils): Add HeaderUtils for header parsing
    This utility class centralizes the extraction of common header values
    like JWT tokens and tenant IDs from HttpServletRequest objects.
    It promotes code reusability and maintainability.

---
+++ src/main/java/org/droid/zero/multitenantaipayrollsystem/system/util/HeaderUtils.java
@@ -0,0 +1,23 @@
+package org.droid.zero.multitenantaipayrollsystem.system.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class HeaderUtils {
+
+    private static final String TENANT_HEADER = "X-Tenant-ID";
+    private static final String AUTHENTICATION_HEADER ="Authorization";
+
+    public static String extractJwt(HttpServletRequest request) {
+        String authHeader = request.getHeader(AUTHENTICATION_HEADER);
+        String token = null;
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+        }
+        return token;
+    }
+
+    public static String extractTenantId(HttpServletRequest request) {
+        return request.getHeader(TENANT_HEADER);
+    }
+
+}
